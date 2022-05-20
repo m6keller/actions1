8f6b2ab1f919a638f203132dcad08d57e1bf4871
m6keller <kellermatthewe@gmail.com>
2022-05-19 20:53:44 -0700

    copies directory

--- utility/copyDirectory.js
+++ utility/copyDirectory.js
@@ -1,6 +1,5 @@
+import * as path from "path";
 import * as fs from "fs";
-import { exec } from "child_process";
-const fsAsync = fs.promises;
 import * as fse from "fs-extra";
 
 const srcDir = `dist`;
@@ -10,9 +9,3 @@ fse
   .copy(srcDir, destDir)
   .then(console.log("done"))
   .catch((error) => console.log(error));
-
-// try {
-//   await fsAsync.copyFile("./dist/index.js", "./actions2/index.js");
-// } catch (error) {
-//   console.log(error);
-// }
